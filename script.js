52188237f40f6d19dbf66c833bbed87765ebd9c4
Yazmeen Vazquez <yazmeen.vazquez14@gmail.com>
2017-03-02 23:32:06 +0000

    this

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+var Code2College_Students = ["Andrea", "Vanessa", "Marlene", "Cindy", "Sophie", "Natalia", "Selena", "Yazmeen"];
 
+$("#studentButton").click(function(){
+    var student = Math.floor(Math.random()*Code2College_Students.length);
+    $("#studentDisplay").html(Code2College_Students[student]);
+});
+
+$("#teacherButton").click(function(){
+    var Code2College_Educators = ["Chris", "Jon", "Matt", "Stephanie"];
+    var teacher = Math.floor(Math.random()*Code2College_Educators.length);
+    $("#teacherDisplay").html(Code2College_Educators[teacher]);
+});
+
+$("#namie").val();
+
+$("#add").click(function(){
+    Code2College_Students.push($("#namie").val());
+    console.log(Code2College_Students)
+});
+//able to add any student you want
 
 });
